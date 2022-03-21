4c9927a4a4a1e686f32ec0472809287f621d0976
USER <USER@LAPTOP-I37ISAN5>
2022-03-20 21:00:31 -0400

    Clase Enfermedad

---
+++ ProyectoFinal/src/logico/Enfermedad.java
@@ -0,0 +1,20 @@
+package logico;
+
+public class Enfermedad {
+	
+	private String enfermedadNombre;
+	
+	public Enfermedad(String enfermedadNombre) {
+		super();
+		this.enfermedadNombre = enfermedadNombre;
+	}
+
+	public String getEnfermedadNombre() {
+		return enfermedadNombre;
+	}
+
+	public void setEnfermedadNombre(String enfermedadNombre) {
+		this.enfermedadNombre = enfermedadNombre;
+	}
+
+}
