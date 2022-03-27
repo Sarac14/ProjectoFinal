dc90f0c59cb9e29c2002b9f05b0e4e6e516b2cce
USER <USER@LAPTOP-I37ISAN5>
2022-03-27 17:08:28 -0400

    Cree CuentaDoctor y ListDocCita

--- ProyectoFinal/src/Visual/ListDocCita.java
+++ ProyectoFinal/src/Visual/ListDocCita.java
@@ -14,28 +14,38 @@ import javax.swing.border.EtchedBorder;
 import javax.swing.border.MatteBorder;
 import java.awt.Color;
 import javax.swing.border.SoftBevelBorder;
+import javax.swing.table.DefaultTableModel;
+
+import logico.Clinica;
+import logico.Doctor;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ScrollPaneConstants;
 
 public class ListDocCita extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-
+	private JTable table;
+	private DefaultTableModel model;
+	private Object row[];
+	private Doctor miDoctor = null;
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		try {
-			ListDocCita dialog = new ListDocCita();
+			ListDocCita dialog = new ListDocCita(null);
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
 	/**
 	 * Create the dialog.
 	 */
-	public ListDocCita() {
+	public ListDocCita(Doctor doctor) {
+		miDoctor = doctor;
 		setBounds(100, 100, 799, 477);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -45,7 +55,19 @@ public class ListDocCita extends JDialog {
 			JPanel panel = new JPanel();
 			panel.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
 			contentPanel.add(panel, BorderLayout.CENTER);
-			panel.setLayout(null);
+			panel.setLayout(new BorderLayout(0, 0));
+			
+			JScrollPane scrollPane = new JScrollPane();
+			scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+			panel.add(scrollPane, BorderLayout.CENTER);
+			{
+				String headers[] = {"Cedula","Nombre","Fecha"};
+				model = new DefaultTableModel();
+				model.setColumnIdentifiers(headers);
+				table = new JTable();
+				table.setModel(model);
+				scrollPane.setViewportView(table);
+			}
 		}
 		{
 			JPanel buttonPane = new JPanel();
@@ -62,6 +84,19 @@ public class ListDocCita extends JDialog {
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
+		}
+		loadTable(miDoctor);
+	}
+
+	private void loadTable(Doctor doctor) {
+
+		model.setRowCount(0);
+		row = new Object[model.getColumnCount()];
+		for(int i = 0; i < doctor.getMisCitas().size(); i++) {
+			row[0] = doctor.getMisCitas().get(i).getCedula();
+			row[1] = doctor.getMisCitas().get(i).getPersona();
+			row[2] = doctor.getMisCitas().get(i).getFecha();
+			model.addRow(row);
 		}
 	}
 }
