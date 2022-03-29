4d2a9517cad7cecc7c0cfba7bebe267f5aeb7151
ediso <ediso@192.168.0.105>
2022-03-29 15:05:50 -0400

    Agregue RegVacuna

---
+++ ProyectoFinal/src/Visual/RegVacuna.java
@@ -0,0 +1,76 @@
+package Visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class RegVacuna extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			RegVacuna dialog = new RegVacuna();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public RegVacuna() {
+		setTitle("Agregar Vacuna");
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JPanel panel = new JPanel();
+			contentPanel.add(panel, BorderLayout.CENTER);
+			panel.setLayout(null);
+			
+			JLabel lblNewLabel = new JLabel("Nombre: ");
+			lblNewLabel.setBounds(12, 13, 56, 16);
+			panel.add(lblNewLabel);
+			
+			textField = new JTextField();
+			textField.setBounds(80, 10, 330, 22);
+			panel.add(textField);
+			textField.setColumns(10);
+			
+			JLabel lblNewLabel_1 = new JLabel("Codigo");
+			lblNewLabel_1.setBounds(12, 52, 56, 16);
+			panel.add(lblNewLabel_1);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+}
--- ProyectoFinal/src/logico/Vacuna.java
+++ ProyectoFinal/src/logico/Vacuna.java
@@ -2,13 +2,22 @@ package logico;
 
 import java.sql.Date;
 
-public class Vacuna extends Proceso {
+public class Vacuna{
 	
+	private String codigo;
 	private String nombre;
+	private Enfermedad enfermedad;
+	private Date fechaCaducidad;
+	private int cantReal;
 
-	public Vacuna(String codigo, String paciente, String doctor, Date fecha, String nombreñ) {
-		super(codigo, paciente, doctor, fecha);
-		this.nombre = nombreñ;
+	
+
+	public Vacuna(String codigo, String nombre, Enfermedad enfermedad, Date fechaCaducidad, int cantReal) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.enfermedad = enfermedad;
+		this.fechaCaducidad = fechaCaducidad;
+		this.cantReal = cantReal;
 	}
 
 	public String getNombre() {
@@ -17,6 +26,38 @@ public class Vacuna extends Proceso {
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public Enfermedad getEnfermedad() {
+		return enfermedad;
+	}
+
+	public void setEnfermedad(Enfermedad enfermedad) {
+		this.enfermedad = enfermedad;
+	}
+
+	public Date getFechaCaducidad() {
+		return fechaCaducidad;
+	}
+
+	public void setFechaCaducidad(Date fechaCaducidad) {
+		this.fechaCaducidad = fechaCaducidad;
+	}
+
+	public int getCantReal() {
+		return cantReal;
+	}
+
+	public void setCantReal(int cantReal) {
+		this.cantReal = cantReal;
 	}
 	
 	
