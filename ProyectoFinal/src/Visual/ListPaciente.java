9967e44af32fd64634031404a8ceaccb7ce9310f
saraa02 <saram.contreras.02rgmail.com>
2022-03-30 23:06:55 -0400

    ListPaciente

--- ProyectoFinal/src/Visual/ListPaciente.java
+++ ProyectoFinal/src/Visual/ListPaciente.java
@@ -7,37 +7,30 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.border.BevelBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
 import logico.Clinica;
 
+import java.awt.Toolkit;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTable;
 import javax.swing.ScrollPaneConstants;
-import javax.swing.border.BevelBorder;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.LineBorder;
-import java.awt.Color;
-import javax.swing.border.MatteBorder;
-import javax.swing.border.TitledBorder;
-import javax.swing.border.SoftBevelBorder;
-import java.awt.Toolkit;
 
-public class ListDoctor extends JDialog {
+public class ListPaciente extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
 	private JTable table;
 	private DefaultTableModel model;
 	private Object row[];
-
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		try {
-			ListDoctor dialog = new ListDoctor();
+			ListPaciente dialog = new ListPaciente();
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
@@ -48,10 +41,10 @@ public class ListDoctor extends JDialog {
 	/**
 	 * Create the dialog.
 	 */
-	public ListDoctor() {
-		setIconImage(Toolkit.getDefaultToolkit().getImage(ListDoctor.class.getResource("/Imagenes/seguro-de-salud.png")));
-		setTitle("Listado de doctores");
-		setBounds(100, 100, 744, 442);
+	public ListPaciente() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(ListPaciente.class.getResource("/Imagenes/seguro-de-salud.png")));
+		setTitle("Listado de Pacientes");
+		setBounds(100, 100, 500, 324);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -67,7 +60,7 @@ public class ListDoctor extends JDialog {
 				scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 				panel.add(scrollPane, BorderLayout.CENTER);
 				{
-					String headers[] = {"Nombre","Cedula","Telefono","Direccion","Especialidad"};
+					String headers[] = {"Nombre","Cedula","Telefono","Direccion","Edad","Peso","Altura"};
 					model = new DefaultTableModel();
 					model.setColumnIdentifiers(headers);
 					table = new JTable();
@@ -75,7 +68,6 @@ public class ListDoctor extends JDialog {
 					scrollPane.setViewportView(table);
 				}
 			}
-
 		}
 		{
 			JPanel buttonPane = new JPanel();
@@ -92,22 +84,30 @@ public class ListDoctor extends JDialog {
 				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
 			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
 		}
 		loadTable();
 	}
 
 	private void loadTable() {
-
 		model.setRowCount(0);
 		row = new Object[model.getColumnCount()];
-		for(int i = 0; i < Clinica.getInstance().getMisDoctores().size(); i++) {
-			row[0] = Clinica.getInstance().getMisDoctores().get(i).getNombre();
-			row[1] = Clinica.getInstance().getMisDoctores().get(i).getCedula();
-			row[2] = Clinica.getInstance().getMisDoctores().get(i).getTelefono();
-			row[3] = Clinica.getInstance().getMisDoctores().get(i).getDireccion();
-			row[4] = Clinica.getInstance().getMisDoctores().get(i).getEspecialidad();
+		for(int i = 0; i < Clinica.getInstance().getMisPacientes().size(); i++) {
+			row[0] = Clinica.getInstance().getMisPacientes().get(i).getNombre();
+			row[1] = Clinica.getInstance().getMisPacientes().get(i).getCedula();
+			row[2] = Clinica.getInstance().getMisPacientes().get(i).getTelefono();
+			row[3] = Clinica.getInstance().getMisPacientes().get(i).getDireccion();
+			row[4] = Clinica.getInstance().getMisPacientes().get(i).getEdad();
+			row[5] = Clinica.getInstance().getMisPacientes().get(i).getPeso();
+			row[6] = Clinica.getInstance().getMisPacientes().get(i).getEstatura();
+
 			model.addRow(row);
+
 		}
 	}
-}
 
+}
