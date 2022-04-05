7df2379d858251d302b3befcced811d993642c17
USER <USER@LAPTOP-I37ISAN5>
2022-04-05 17:03:40 -0400

    Modifique Doctor

--- ProyectoFinal/src/logico/Doctor.java
+++ ProyectoFinal/src/logico/Doctor.java
@@ -5,13 +5,18 @@ import java.util.ArrayList;
 public class Doctor extends Persona {
 	private String Especialidad;
 	private ArrayList<Cita>misCitas;
+	private String usuarioDoctor;
+	private String contrasenaDoctor;
 	
-	public Doctor(String cedula, String nombre, String dirrecion, String telefono, char sexo, String especialidad) {
-		super(cedula, nombre, dirrecion, telefono, sexo);
+	public Doctor(String cedula, String nombre, String direccion, String telefono, char sexo, String especialidad,
+			ArrayList<Cita> misCitas, String usuarioDoctor, String contrasenaDoctor) {
+		super(cedula, nombre, direccion, telefono, sexo);
 		Especialidad = especialidad;
-		this.misCitas = new ArrayList<>();
+		this.misCitas = misCitas;
+		this.usuarioDoctor = usuarioDoctor;
+		this.contrasenaDoctor = contrasenaDoctor;
 	}
-	
+
 	public String getEspecialidad() {
 		return Especialidad;
 	}
@@ -23,6 +28,22 @@ public class Doctor extends Persona {
 	}
 	public void setMisCitas(ArrayList<Cita> misCitas) {
 		this.misCitas = misCitas;
+	}
+
+	public String getUsuarioDoctor() {
+		return usuarioDoctor;
+	}
+
+	public void setUsuarioDoctor(String usuarioDoctor) {
+		this.usuarioDoctor = usuarioDoctor;
+	}
+
+	public String getContrasenaDoctor() {
+		return contrasenaDoctor;
+	}
+
+	public void setContrasenaDoctor(String contrasenaDoctor) {
+		this.contrasenaDoctor = contrasenaDoctor;
 	}
 
 }
